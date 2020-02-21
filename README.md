2e70878b3d5d01de68bb04a93bbb35a929c2e5e5
Игорь Потапов <igor@potapoff.org>
2020-02-21 15:52:27 +0300

    -

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+git clone git@github.com:SkAZi/podcast.git
+```
--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #coding:utf-8
 
-from bottle import Bottle, get, template, response
+from bottle import Bottle, get, template, response, static_file
 from datetime import datetime
 from yaml import load
 import os
@@ -13,34 +13,42 @@ except ImportError:
 app = Bottle()
 
 def refresh():
+    data = load(open('data/data.yaml', 'r'), Loader=Loader)
     ret = []
-    for item in load(open('data/data.yaml', 'r'), Loader=Loader):
+    for item in data['stream']:
         try:
             size = os.path.getsize(os.path.join('data/files', item['file']))
             item['size'] = size
             ret.append(item)
         except:
             pass
-    return ret
+    return (ret, data['meta'])
 
-CACHE, TIME = [], None
+CACHE, TIME, META = [], None, {}
 def get_data():
     global CACHE
     global TIME
+    global META
     if not TIME or (datetime.now() - TIME).seconds > 60:
         TIME = datetime.now()
-        CACHE = refresh()
+        (CACHE, META) = refresh()
     return CACHE
 
 @app.get('/itunes')
 def feed():
+    global META
     response.content_type = 'application/rss+xml; charset=UTF-8'
-    return template('templates/rss.xml', items=get_data())
+    return template('templates/rss.xml', meta=META, items=get_data())
 
 @app.get('/itunes/preview')
 def preview():
+    global META
     response.content_type = 'application/rss+xml; charset=UTF-8'
-    return template('templates/rss.xml', items=refresh())
+    return template('templates/rss.xml', meta=META, items=refresh())
+
+@app.route('/feed/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='/root/code/data/files')
 
 if __name__== "__main__":
     app.run(host='0.0.0.0', server='gunicorn', port=8000)
